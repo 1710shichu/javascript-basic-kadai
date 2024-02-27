cc91761b1f8489afa61adc32ad4b4f7c2b7fbdf0
1710shichu <157471262+1710shichu@users.noreply.github.com>
2024-02-27 14:29:45 +0900

    teishutu2

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -4,8 +4,8 @@ const month = today.getMonth()+1;
 const date = today.getDate();
 const year = today.getFullYear();
 
-console.log(year);
-console.log(month);
-console.log(date);
+console.log(year+'年');
+console.log(month+'月');
+console.log(date+'日');
 
 
