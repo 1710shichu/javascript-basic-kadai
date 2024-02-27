4e8ab9b47c60aa61ef7ebef5091a26f3ab2fc4b2
1710shichu <157471262+1710shichu@users.noreply.github.com>
2024-02-28 01:01:32 +0900

    teishutu

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const changeBtn = document.getElementById('btn');
+
+const Text = document.getElementById('text');
+
+changeBtn.addEventListener('click', () =>{
+  Text.textContent ='ボタンをクリックしました';
+});
